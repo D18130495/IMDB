0ecb88e00107451e65f3599ef28da35f2da17d36
D18130495 <d18130495@mytudublin.ie>
2022-09-27 23:53:59 +0100

    Created user-movie star matrix

--- src/service/recommendation.py
+++ src/service/recommendation.py
@@ -1,9 +1,11 @@
+import time
+
 import pandas as pd
 from math import *
 import numpy as np
 
 
-def merge_data(url):
+def merge_data():
     movies = pd.read_csv("../../ml-latest-small/movies.csv")
     ratings = pd.read_csv("../../ml-latest-small/ratings.csv")
     data = pd.merge(movies, ratings, on='movieId')
@@ -13,8 +15,21 @@ def merge_data(url):
         if data["title"][line][-12:-7] == ", The":
             data.loc[line, "title"] = "The " + data["title"][line].replace(", The", "")
 
-    data.to_csv('./ml-latest-small/data.csv', index=False)
+    data.to_csv('../../ml-latest-small/data.csv', index=False)
+
+
+def matrix():
+    file = open('../../ml-latest-small/data.csv', 'r', encoding='UTF-8')
+    data = {}
+
+    for line in file.readlines():
+        line = line.strip().split(',')
+        if not line[0] in data.keys():
+            data[line[0]] = {line[3]: line[1]}
+        else:
+            data[line[0]][line[3]] = line[1]
 
 
 if __name__ == '__main__':
-    merge_data()
+    # merge_data()
+    matrix()
